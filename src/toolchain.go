ea928b857ee7150df2dfa0c665a99d2b1017bff4
Christopher Norton <24207721+cnorton-webdev@users.noreply.github.com>
2022-08-28 11:34:43 -0400

    Fix memory buffer on Windows if cache path not on C: volume
    On Windows machines if buffer to memory is active and the xTeVe cache is set to a folder on a volume other than C: it will cause a panic error and the stream not to play.
    
    This fixes that by checking if we are on Windows and if so is the cache folder path not on the C: volume, if not create the volume it is located on in the memory virtual file system.

--- src/toolchain.go
+++ src/toolchain.go
@@ -59,6 +59,14 @@ func checkVFSFolder(path string, vfs avfs.VFS) (err error) {
 	if fsIsNotExistErr(err) {
 		// Folder does not exist, will now be created
 
+		// If we are on Windows and the cache location path is NOT on C:\ we need to create the volume it is located on
+		// Failure to do so here will result in a panic error and the stream not playing
+		vm := vfs.(avfs.VolumeManager)
+		vfsUtils := avfs.NewUtils(vfs.OSType())
+		if vfs.OSType() == avfs.OsWindows && vfsUtils.VolumeName(path) != "C:" {
+			vm.VolumeAdd(path)
+		}
+
 		err = vfs.MkdirAll(getPlatformPath(path), 0755)
 		if err == nil {
 
